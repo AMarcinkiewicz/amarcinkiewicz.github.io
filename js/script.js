3fbd0a698de8815e216e6617c2f2de55ef1dc5b9
amarcinkiewicz <marcinkiewicz.alex@gmail.com>
2019-10-19 21:49:56 -0400

    Dark Mode Update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$('body').toggleClass(localStorage.toggled);
+
+function darkLight() {
+  if (localStorage.toggled != 'dark_mode') {
+    $('body').toggleClass('dark_mode', true);
+    localStorage.toggled = "dark_mode";
+
+  } else {
+    $('body').toggleClass('dark_mode', false);
+    localStorage.toggled = "";
+  }
+}
+
+if ($('body').hasClass('dark_mode')) {
+   $( '#switchbox' ).prop( "checked", true )
+} else {
+  $( '#switchbox' ).prop( "checked", false )
+}
+
+$(document).on('click', '.dropdown-menu', function (e) {
+  e.stopPropagation();
+});
